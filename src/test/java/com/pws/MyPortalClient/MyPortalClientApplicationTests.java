8837ac64fbbbae8770bb3de75251a3dbe556ad5e
Kavyans18 <kavyans1804@gmail.com>
2023-03-28 17:20:01 +0530

    Client application

---
+++ src/test/java/com/pws/MyPortalClient/MyPortalClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pws.MyPortalClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortalClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
